358342702f305f61b9c76062248ddc412b5d158b
BluSkink <bjkoehling@gmail.com>
2025-04-15 19:03:47 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# Wake word used to activate the assistant
+WAKE_WORD = "hey assistant"
+
+# OpenWeatherMap API key (obtain it from https://openweathermap.org/api)
+OPENWEATHER_API_KEY = "your_api_key_here"
